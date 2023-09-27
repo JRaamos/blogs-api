f5fd3e57e5ed47bd7c155e9442cf51367c002b56
JRaamos <jhonyramos46@gmail.com>
2023-09-27 17:49:11 -0300

    fix: resolver erro de lint

--- src/middlewares/validateLogin.js
+++ src/middlewares/validateLogin.js
@@ -1,6 +1,5 @@
 const validateLogin = (req, res, next) => {
   const { email, password } = req.body;
-  const regexEmail = /[A-Za-z]+@[A-Za-z]+\.com/;
   if (!email || !password) {
     return res.status(400).json({ message: 'Some required fields are missing' });
   }
