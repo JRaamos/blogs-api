5621a404e3482729db89438b1a6fc73651212bcc
JRaamos <jhonyramos46@gmail.com>
2023-09-28 00:43:27 -0300

    feat: 11 - Cria o modelo PostCategory

---
+++ src/models/PostCategory.js
@@ -0,0 +1,35 @@
+module.exports = (sequelize, DataTypes) => {
+  const PostCategory = sequelize.define('PostCategory', {
+    postId: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      fild: 'post_id',
+    },
+    categoryId: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      fild: 'category_id',
+    },
+  }, {
+    timestamps: false,
+    tableName: 'PostsCategories',
+    underscored: true,
+  });
+
+  PostCategory.associate = (models) => {
+    models.BlogPost.belongsToMany(models.Category, {
+      as: 'categories',
+      through: PostCategory,
+      foreignKey: 'postId',
+      otherKey: 'categoryId',
+    });
+    models.Category.belongsToMany(models.BlogPost, {
+      as: 'blogPosts',
+      through: PostCategory,
+      foreignKey: 'categoryId',
+      otherKey: 'postId',
+    });
+  };
+
+  return PostCategory;
+}
